e0ae7a1a4552371b338f413e4c7a1852460c7f93
CrDanil <zaborovschidanil@gmail.com>
2025-06-10 11:32:02 +0300

    fith

--- tests/tests/base2.ts
+++ tests/tests/base2.ts
@@ -25,3 +25,37 @@ export function toCamelCase(str: string): string { // camelCase
         .join('');
 }
 console.log(toCamelCase("I-Am-a-programmer"));
+
+export function humanReadable(seconds:number):string { // секунды в ХХ:ХХ:ХХ
+    function zero(n:number):string {
+        return n.toString().padStart(2, '0');
+    }
+    const humanMas: string [] = [];
+    const sec2 = zero(seconds%60)
+    const minute = zero(Math.floor(seconds/60)%60)
+    const hour = zero(Math.floor((seconds/60)/60))
+    humanMas.push(hour+":"+minute+":"+sec2)
+    return humanMas.toString()
+}
+console.log(humanReadable(30600))
+
+
+
+
+function transform(str: string, from: string[], to: string[]): string { //code - decode
+    const map: Record<string, string> = {};
+    for (let i = 0; i < from.length; i++) {
+        map[from[i]] = to[i];
+    }
+    return str.split("").map(ch => map[ch] || ch).join("");
+}
+
+export function encode(str: string): string {
+    return transform(str, ["a", "e", "i", "o", "u"], ["1", "2", "3", "4", "5"]);
+}
+
+export function decode(str: string): string {
+    return transform(str, ["1", "2", "3", "4", "5"], ["a", "e", "i", "o", "u"]);
+}
+console.log(encode("Hello"))
+console.log(decode("h2ll4"))
