1db67bffac6a9485439c074cbd588038fd1168af
CrDanil <zaborovschidanil@gmail.com>
2025-06-12 16:35:45 +0300

    tenth

--- e2e/hw2.spec.ts
+++ e2e/hw2.spec.ts
@@ -1,11 +1,29 @@
-const {test, expect} = require('@playwright/test');
-test('hw2', async ({ page }) => {
-    await page.goto('https://demoqa.com/automation-practice-form');
-    await page.locator('//*[@id="firstName"]').fill("Danila");
-    await page.locator('//*[@id="lastName"]').fill("Z");
-    await page.locator('//*[@id="userEmail"]').fill("Danila@mail.ru");
-    await page.locator('//*[@id="genterWrapper"]/div[2]/div[1]/label').click();
-    await page.getByRole('textbox', { name: 'Mobile Number' }).fill("1234567890");
-    await page.getByRole('button', {name: 'Submit'}).click();
+const { test, expect } = require('@playwright/test');
 
-})
+let shouldRunUITest = false;
+
+test('GET check', async ({ request }) => {
+    const response = await request.get('https://demoqa.com/automation-practice-form');
+    expect(response.status()).toBe(200);
+    shouldRunUITest = true; // пометка, что можно идти в UI
+});
+
+test('UI-test', async ({ page }) => {
+    test.skip(!shouldRunUITest, 'Форма недоступна — UI-тест пропущен');
+
+    await page.goto('https://demoqa.com/automation-practice-form');
+
+    await page.locator('#firstName').fill("Danila"); //CSS
+    await page.locator('//*[@id="firstName"]').fill('Danila'); //xPath
+
+    await page.locator('#lastName').fill("Z");
+    await page.locator('#userEmail').fill("Danila@mail.ru");
+
+    await expect(page.locator('#gender-radio-1').isChecked())
+
+    await page.locator('label[for="gender-radio-1"]').click(); //CSS
+    await page.locator('//*[@id="genterWrapper"]/div[2]/div[1]/label').click(); //xPath
+
+    await page.getByRole('textbox', { name: 'Mobile Number' }).fill("1234567890");
+    await page.getByRole('button', { name: 'Submit' }).click();
+});
